11e3e0919775e67a9f41fc319abdb9a49ee87585
linucksrox <ericdalyblog@dalydays.com>
2021-06-11 17:09:57 +0000

    Add sample Nextcloud config directive for reference

---
+++ nextcloud-config-onlyoffice-sample.php
@@ -0,0 +1,5 @@
+// add this array to Nextcloud's config.php to match up the JWT auhtorization header with what is defined in stack.yml
+'onlyoffice' => 
+  array (
+    'jwt_header' => 'NCAuth',
+  ),
